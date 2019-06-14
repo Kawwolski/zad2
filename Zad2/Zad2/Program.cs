986634cc573548649da92cc892ff63ec226e019c
Kawwolski <47087721+Kawwolski@users.noreply.github.com>
2019-06-14 15:30:44 +0200

    Add files via upload

---
+++ Zad2/Zad2/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Temperature temp = new Temperature();
+            Console.WriteLine("1.Podaj temperature w Kelwinach");
+            Console.WriteLine("2.Zapisz temperature w Kelwinach");
+            Console.WriteLine("3.Podaj obecna temperature w Celcjusza");
+            Console.WriteLine("4.Zapisz temperature Celcjusza w Kelwinach");
+            Console.WriteLine("5.Podaj obecna temperature w Fahrenheitach");
+            Console.WriteLine("6.Zapisz temperature Fahrenheita w Kelwinach");
+            ConsoleKeyInfo k = new ConsoleKeyInfo();
+            k = Console.ReadKey();
+            double kel;
+            do
+            {
+                switch (k.Key)
+                {
+                    case ConsoleKey.D1:
+                        Console.WriteLine("\nObecna temperatura wyrazona w Kelwinach rowna sie:" + temp.Getkelwintemp());
+                        Console.WriteLine("Wybierz inna opcje");
+                        k = Console.ReadKey();
+                        continue;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("\nPodaj temperature w Kelwinach");
+                        kel = double.Parse(Console.ReadLine());
+                        temp.Setkelwintemp(kel);
+                        Console.WriteLine("Obecna temperatura wyrazona w Kelwinach rowna sie:" + temp.Getkelwintemp());
+                        Console.WriteLine("Wybierz inna opcje");
+                        k = Console.ReadKey();
+                        continue;
+                    case ConsoleKey.D3:
+                        Console.WriteLine("\nObecna temperatura wyrazona w Kelwinach rowna sie:" + temp.Getkelwintemp());
+                        Console.WriteLine("Obecna temperatura wyrazona w Celcjuszach rowna sie:" + temp.GetCeltemp());
+                        Console.WriteLine("Wybierz inna opcje");
+                        k = Console.ReadKey();
+                        continue;
+                    case ConsoleKey.D4:
+                        Console.WriteLine("\nPodaj temperature w Cel: ");
+                        kel = double.Parse(Console.ReadLine());
+                        temp.Setkelfromcel(kel);
+                        Console.WriteLine("Temperatura w Kelwinach: " + temp.Getkelwintemp());
+                        Console.WriteLine("Wybierz inna opcje");
+                        k = Console.ReadKey();
+                        continue;
+                    case ConsoleKey.D5:
+                        Console.WriteLine("\ntemperatura w Kelwinach: " + temp.Getkelwintemp());
+                        Console.WriteLine("Temperatura w Fahrenheitach: " + temp.Getfartemp());
+                        Console.WriteLine("Wybierz inna opcje");
+                        k = Console.ReadKey();
+                        continue;
+                    case ConsoleKey.D6:
+                        Console.WriteLine("\nPodaj temperature w Fahrenheitach: ");
+                        kel = double.Parse(Console.ReadLine());
+                        temp.Setkelfromfar(kel);
+                        Console.WriteLine("Temperatura w Kelwinach: " + temp.Getkelwintemp());
+                        Console.WriteLine("Wybierz inna opcje");
+                        k = Console.ReadKey();
+                        continue;
+                }
+            } while (k.Key != ConsoleKey.Escape);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Zad2/Zad2/Temperature.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zad2
+{
+    class Temperature
+    {
+        double kelwintemp=0;
+
+        public double Getkelwintemp()
+        {
+            return kelwintemp;
+        }
+        public void Setkelwintemp(double K)
+        {
+            this.kelwintemp = K;
+        }
+
+        public double GetCeltemp()
+        {
+            return kelwintemp - 273.15;
+        }
+        public void Setkelfromcel(double c)
+        {
+            this.kelwintemp = c + 273.15;
+        }
+
+        public double Getfartemp()
+        {
+            return (GetCeltemp() * 1.8) + 32;
+        }
+
+        public void Setkelfromfar(double f)
+        {
+            this.kelwintemp = ((f-32)/1.8)+273.15;
+        }
+
+    }
+}
